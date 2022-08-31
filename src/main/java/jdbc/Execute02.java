792804151d97076eaf82d2ac6a3cb0ad927a524e
Emre Sulukan <esulukan34@gmail.com>
2022-08-31 23:53:07 +0300

    22.08.31

--- src/main/java/jdbc/Execute02.java
+++ src/main/java/jdbc/Execute02.java
@@ -8,7 +8,9 @@ public class Execute02 {
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed","postgres",   "2101emre");
         Statement st = con.createStatement();
+
         //1. Example:  region id'si 1 olan "country name" değerlerini çağırın.
+
         String sql1 = "SELECT country_name FROM countries WHERE region_id = 1";
         boolean r1 = st.execute(sql1);
         System.out.println(r1);// true yada false verir. Cunku data cagirma islemi yaptik
@@ -24,6 +26,7 @@ public class Execute02 {
         System.out.println("***************");
 
         //2.Örnek: "region_id"nin 2'den büyük olduğu "country_id" ve "country_name" değerlerini çağırın.
+
         String sql2 = "SELECT country_name,country_id FROM countries WHERE region_id > 2";
         ResultSet result2 = st.executeQuery(sql2);
         while(result2.next()){ // siradaki datayi gosterene kadar calisir
@@ -33,12 +36,15 @@ public class Execute02 {
 
         System.out.println("***************");
 
+        //3.Örnek: "number_of_employees" değeri en düşük olan satırın tüm değerlerini çağırın.
 
         String sql3 = "SELECT * FROM companies WHERE number_of_employees = (SELECT MIN(number_of_employees) FROM companies)";
         ResultSet result3 = st.executeQuery(sql3);
 
         while (result3.next()){
             System.out.println(result3.getInt("company_id")+"--"+result3.getString("company")+"--"+result3.getInt("number_of_employees"));
+
+            // veya index ile ayni sonuc gelir:
             System.out.println(result3.getInt(1)+"--"+result3.getString(2)+"--"+result3.getInt(3));
 
         }
